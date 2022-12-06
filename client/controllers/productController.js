ca5cb8888ef68994cd22a47ae61e17c5c21a0e1a
Le Hoai <volehoai070902@gmail.com>
2022-12-07 01:28:21 +0700

    getbyCategory

--- client/controllers/productController.js
+++ client/controllers/productController.js
@@ -3,55 +3,105 @@ const Category = require("../models/Category");
 const utils = require("../utils/mongoose");
 const utilsPagination = require("../utils/pagination");
 
-
 async function getAllProduct(req, res) {
-    let perPage = 6,
-        page = Math.max(parseInt(req.param("page")) || 1, 1);
-    if (req.param("page") == null) {
-        page = 1;
-    }
+  let perPage = 6,
+    page = Math.max(parseInt(req.param("page")) || 1, 1);
+  if (req.param("page") == null) {
+    page = 1;
+  }
 
-    const products = await Product.find({})
-        .skip(perPage * (page - 1))
-        .limit(perPage);
+  const products = await Product.find({})
+    .skip(perPage * (page - 1))
+    .limit(perPage);
 
-    const size = await Product.count({});
-    const sizePage = Math.max(parseInt(size / perPage + 1));
+  const size = await Product.count({});
+  const sizePage = Math.max(parseInt(size / perPage + 1));
 
-    const categories = await Category.find({});
-    const leftPage = await utilsPagination.getLeftPage(
-        "/shop-grid",
-        page,
-        sizePage
-    );
-    const pagination = await utilsPagination.getPagination(
-        "/shop-grid",
-        page,
-        sizePage
-    );
-    const rightPage = await utilsPagination.getRightPage(
-        "/shop-grid",
-        page,
-        sizePage
-    );
+  const categories = await Category.find({});
+  const leftPage = await utilsPagination.getLeftPage(
+    "/shop-grid",
+    page,
+    sizePage
+  );
+  const pagination = await utilsPagination.getPagination(
+    "/shop-grid",
+    page,
+    sizePage
+  );
+  const rightPage = await utilsPagination.getRightPage(
+    "/shop-grid",
+    page,
+    sizePage
+  );
 
-    let latestProducts = await Product.find({});
-    latestProducts = latestProducts.slice(0, 3);
+  let latestProducts = await Product.find({});
+  latestProducts = latestProducts.slice(0, 3);
 
-    res.render("shop-grid/shop-grid", {
-        products: utils.mutipleMongooseToObject(products),
-        size: size,
-        currentPage: page,
-        category: utils.mutipleMongooseToObject(categories),
-        pagination: pagination,
-        leftPage: leftPage,
-        rightPage: rightPage,
-        latestProducts: utils.mutipleMongooseToObject(latestProducts),
-    });
+  res.render("shop-grid/shop-grid", {
+    products: utils.mutipleMongooseToObject(products),
+    size: size,
+    currentPage: page,
+    category: utils.mutipleMongooseToObject(categories),
+    pagination: pagination,
+    leftPage: leftPage,
+    rightPage: rightPage,
+    latestProducts: utils.mutipleMongooseToObject(latestProducts),
+  });
 }
 
+//Get/:idCategory
+async function getbyCategory(req, res) {
+  let perPage = 6,
+    page = Math.max(parseInt(req.param("page")) || 1, 1);
+  if (req.param("page") == null) {
+    page = 1;
+  }
+  console.log("hello");
+  let idCategory = req.param("idCategory");
+  console.log(idCategory);
+  let name_Category = await Category.findOne({"idCategory": idCategory});
+  
+  const products = await Product.find({
+    "category": name_Category.name})
+    .skip(perPage * (page - 1))
+    .limit(perPage);
 
+  const size = await Product.count({});
+  const sizePage = Math.max(parseInt(size / perPage + 1));
+
+  const categories = await Category.find({});
+  const leftPage = await utilsPagination.getLeftPage(
+    "/shop-grid",
+    page,
+    sizePage
+  );
+  const pagination = await utilsPagination.getPagination(
+    "/shop-grid",
+    page,
+    sizePage
+  );
+  const rightPage = await utilsPagination.getRightPage(
+    "/shop-grid",
+    page,
+    sizePage
+  );
+
+  let latestProducts = await Product.find({});
+  latestProducts = latestProducts.slice(0, 3);
+
+  res.render("shop-grid/shop-grid", {
+    products: utils.mutipleMongooseToObject(products),
+    size: size,
+    currentPage: page,
+    category: utils.mutipleMongooseToObject(categories),
+    pagination: pagination,
+    leftPage: leftPage,
+    rightPage: rightPage,
+    latestProducts: utils.mutipleMongooseToObject(latestProducts),
+  });
+}
 
 module.exports = {
-    getAllProduct
-}
+  getAllProduct,
+  getbyCategory,
+};
--- client/routes/productRouter.js
+++ client/routes/productRouter.js
@@ -4,5 +4,5 @@ const product_controller = require("../controllers/productController");
 
 router.get("/",product_controller.getAllProduct);
 // router.post("/", loginController.loginPOST);
-
+router.get("/:idCategory", product_controller.getbyCategory);
 module.exports = router;
